3554cede990597f838e7f0809c902678128645f9
Jarrell Waggoner <malloc47@gmail.com>
2011-11-15 00:03:59 -0500

    updated with better error handling and support for clone now also, python 2.X support now in place

--- git.py
+++ git.py
@@ -1,13 +1,25 @@
 import subprocess as sp
 
 def hashes():
-    return sp.check_output(["git","log","--pretty=format:%H"]).decode("utf-8").split('\n')
+    try:
+        return sp.check_output(["git","log","--pretty=format:%H"]).decode("utf-8").split('\n')
+    except sp.CalledProcessError:
+        return []
 
 def last_hash():
-    return sp.check_output("git log --pretty=format:%H | tail -n 1",shell=True).decode("utf-8").strip()
+    try:
+        return sp.check_output("git log --pretty=format:%H | tail -n 1",shell=True).decode("utf-8").strip()
+    except:
+        return None
 
 def name():
-    return sp.check_output("basename $(git rev-parse --show-toplevel)",shell=True).decode("utf-8").strip()
+    try:
+        return sp.check_output("basename $(git rev-parse --show-toplevel)",shell=True).decode("utf-8").strip()
+    except:
+        return None
+
+def exists():
+    return not sp.call("git rev-parse --git-dir &>/dev/null",shell=True)
 
 def cmd(cmd,source):
-    sp.check_call(["git",cmd,source])
+    sp.call(["git",cmd,source])
