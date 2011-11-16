275d83ef59e0113c620804d7cb69bda36b16be84
Jarrell Waggoner <malloc47@gmail.com>
2011-11-16 00:40:02 -0500

    reworked error handling (again), arguments to subfunctions now in dict

--- git.py
+++ git.py
@@ -1,8 +1,9 @@
+import os
 import subprocess as sp
 
 def hashes():
     try:
-        return sp.check_output(["git","log","--pretty=format:%H"]).decode("utf-8").split('\n')
+        return sp.check_output(["git","log","--pretty=format:%H"]).decode("utf-8").split('\n') # todo: reverse this, since we generally store the last hash
     except sp.CalledProcessError:
         return []
 
@@ -19,7 +20,11 @@ def name():
         return None
 
 def exists():
-    return not sp.call("git rev-parse --git-dir &>/dev/null",shell=True)
+    # trick so we don't see output
+    fnull = open(os.devnull, 'w')
+    output = not sp.call(["git","rev-parse","--git-dir"], stdout = fnull, stderr = fnull)
+    fnull.close()
+    return output
 
 def cmd(cmd,source):
     sp.call(["git",cmd,source])
